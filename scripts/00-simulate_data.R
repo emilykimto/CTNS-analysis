65187cf8c99f981b5585b029edf9d297fbf3ec03
Emily Kim <emilyuna.kim@mail.utoronto.ca>
2023-04-17 23:09:47 -0400

    push simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,93 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates relevant data from the 2021 Canadian Tobacco and Nicotine Survey which can be downloaded from https://www150.statcan.gc.ca/n1/pub/13-25-0001/132500012022001-eng.htm
+# Author: Emily Kim
+# Data: 17 April 2023
+# Contact: emilyuna.kim@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: To understand data need to know where to find the 2021 Canadian Tobacco and Nicotine Survey data
 
 #### Workspace setup ####
-library(tidyverse)
-# [...UPDATE THIS...]
+library(tibble)
+library(readr)
+library(here)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
+set.seed(853)
 
+simulated_ctns_2021 <-
+  tibble(
+    "age_group" = sample(
+      x = c(
+        "15 to 19 years old",
+        "20 to 24 years old",
+        "25 to 34 years old",
+        "35 to 44 years old",
+        "45 to 54 years old",
+        "55 to 64 years old",
+        "65 years old and older"
+      ),
+      size = 10000,
+      replace = TRUE
+    ),
+    "gender" = sample(
+      x = c(
+        "Male",
+        "Female",
+        "Not stated"
+      ),
+      size = 10000,
+      replace = TRUE
+    ),
+    "province_of_residence" = sample(
+      x = c(
+        "Newfoundland and Labrador",
+        "Prince Edward Island",
+        "Nova Scotia",
+        "New Brunswick",
+        "Quebec",
+        "Ontario",
+        "Manitoba",
+        "Saskatchewan",
+        "Alberta",
+        "British Columbia"
+      ),
+      size = 10000,
+      replace = TRUE
+    ),
+    "frequency_smoked_cannabis" = sample(
+      x = c(
+        "Daily",
+        "Less than daily, but at least once a week",
+        "Less than once a week, but at least once",
+        "In the past month",
+        "Not at all"
+      ),
+      size = 10000,
+      replace = TRUE
+    ),
+    "frequency_vaped_cannabis" = sample(
+      x = c(
+        "Daily",
+        "Less than daily, but at least once a week",
+        "Less than once a week, but at least once",
+        "In the past month",
+        "Not at all"
+      ),
+      size = 10000,
+      replace = TRUE
+    ),
+    "frequency_consumed_edibles" = sample(
+      x = c(
+        "At least once in the past 30 days",
+        "Not at all"
+      ),
+      size = 10000,
+      replace = TRUE
+    ),
+    
+  )
+
+#### Save Simulated Data ####
+write_csv(simulated_ctns_2021, here::here("inputs/simulated_ctns_2021.csv")) #download to inputs/data
 
