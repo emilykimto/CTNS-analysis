24f82aff672d4a0b2dcc148e44cef7d28db37bfe
Emily Kim <emilyuna.kim@mail.utoronto.ca>
2023-04-18 00:04:08 -0400

    push tests

--- scripts/02-test_data.R
+++ scripts/02-test_data.R
@@ -1,15 +1,28 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Test the simulated data of the 2021 Canadian Tobacco and Nicotine Survey (CTNS). The real data can be downloaded from https://www150.statcan.gc.ca/n1/pub/13-25-0001/132500012022001-eng.htm
+# Author: Emily Kim
+# Data: 17 April 2023
+# Contact: emilyuna.kim@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Need to be able to load in simulated data
 
 #### Workspace setup ####
-library(tidyverse)
-# [...UPDATE THIS...]
+library(here)
 
-#### Test data ####
+#### Load in Simulated Data ####
+simulated_ctns_2021 <- read.csv(here::here("./inputs/simulated_ctns_2021.csv"))
+
+#### Testing Simulated Data ####
+
+#### Test 1 ####
+simulated_ctns_2021$"age_group" |>
+  unique()|>
+  length() == 7
+
+#### Test 2 ####
+simulated_ctns_2021$"frequency_smoked_cannabis" |>
+  class() == "character"
+
+#### Test 3 ####
+simulated_ctns_2021$"gender" |>
+  unique()
